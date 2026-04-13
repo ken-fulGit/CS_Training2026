a0f5c8be1219a1251902ece0fdb167d082cc0692
ken-fulGit <fnsOP2018003@gmail.com>
2026-04-13 21:30:02 +0900

    演習02まで

--- Ex_02_08/Program.cs
+++ Ex_02_08/Program.cs
@@ -4,6 +4,15 @@ static class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var stations = new[]{"品川","大崎","五反田","目黒","恵比寿","渋谷","原宿","代々木","新宿"};
+        //stations.ToList().ForEach(x => Console.WriteLine(x));
+        Console.WriteLine("駅名を入力して下さい");
+        var input = Console.ReadLine();
+        foreach (var station in stations) {
+            if(station == input) {
+                continue;
+            }
+            Console.WriteLine(station);
+        }
     }
 }
---
+++ Ex_02_09/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Ex_02_09;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // PrintHello();
+
+        // int answer = Add(4,5);
+        int answer = Mul3(4);
+        Console.WriteLine(answer);
+    }
+    static int Mul3(int x) {
+        return x * 3;
+    }
+    // static void PrintHello()
+    // {
+    //     Console.WriteLine("Hello, World!");
+    // }
+
+    // static int Add(int x,int y)
+    // {
+    //     return x + y;
+    // }
+}
